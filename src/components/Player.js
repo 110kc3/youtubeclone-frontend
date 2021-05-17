04e22adee7b87ff5376a584656a40e61458e6a21
i529998 <patryk.musiol@sap.com>
2021-05-17 19:38:49 +0200

    ble

---
+++ src/components/Player.js
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import videojs from "video.js";
+import { client } from "../utils";
+import "video.js/dist/video-js.css";
+
+const Player = ({ previewUrl, speedInit }) => {
+  const videoRef = useRef(null);
+  let nSpeed = Math.round(speedInit);
+
+
+  const dispatch = useDispatch();
+  let { id: videoId, url: src, thumb: poster, duration: duration } = useSelector(
+    (state) => state.video.data
+  );
+
+  const fetchSpeed = async () => {
+    const { data } = await client(`${process.env.REACT_APP_BE}/videos/speed`);
+    const vjsPlayer = videojs(videoRef.current);
+    nSpeed=Math.round(data);
+    console.log(nSpeed, src);
+    console.log(vjsPlayer.currentTime())
+    let a = vjsPlayer.currentTime()
+    src = "https://res.cloudinary.com/dtelr3fsv/video/upload/q_1/v1619367127/youtube/qgtotofykmh7q8h7rnqd.mp4"
+    
+    vjsPlayer.src(src);
+    vjsPlayer.currentTime(a);
+    vjsPlayer.play();
+
+
+  }
+
+
+  useEffect(() => {    
+    const interval = setInterval(() => {
+      fetchSpeed();
+      console.log("Network speed: " + nSpeed)
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+  useEffect(() => {
+    const vjsPlayer = videojs(videoRef.current);
+    //SPEED TEST NA PCOZATKU PUSZCZANIA FILMU
+    console.log("Kasia jest OP")
+
+    
+    if (!previewUrl) {
+      vjsPlayer.poster(poster);
+      vjsPlayer.src(src);
+
+    }
+
+    if (previewUrl) {
+      vjsPlayer.src({ type: "video/mp4", src: previewUrl });
+    }
+
+    vjsPlayer.on("ended", () => {
+      client(`${process.env.REACT_APP_BE}/videos/${videoId}/view`);
+    });
+
+    return () => { 
+      if (vjsPlayer) {
+        vjsPlayer.dispose();
+      }
+    };
+  }, [videoId, dispatch, src, previewUrl, poster]);
+
+
+  return (
+    <div data-vjs-player>
+      <video
+        controls
+        id="video"
+        ref={videoRef}
+        className="video-js vjs-fluid vjs-big-play-centered"
+      ></video>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/components/UploadVideo.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import path from "path";
+import { toast } from "react-toastify";
+import { UploadIcon } from "./Icons";
+import UploadVideoModal from "./UploadVideoModal";
+import { upload } from "../utils";
+
+const UploadVideo = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [previewVideo, setPreviewVideo] = useState("");
+  const closeModal = () => setShowModal(false);
+
+  const [url, setUrl] = useState("");
+
+  const [duration, setDuration] = useState(0);
+  const [thumbnail, setThumbnail] = useState("");
+
+  const loadVideo = file => new Promise((resolve, reject) => {
+      try {
+          let video = document.createElement('video')
+          video.preload = 'metadata'
+
+          video.onloadedmetadata = function () {
+              resolve(this)
+          }
+
+          video.onerror = function () {
+              reject("Invalid video. Please select a video file.")
+          }
+
+          video.src = window.URL.createObjectURL(file)
+      } catch (e) {
+          reject(e)
+      }
+  })
+
+  const handleVideoUpload = async (e) => {
+    const file = e.target.files[0];
+    const video = await loadVideo(e.currentTarget.files[0])
+
+    if (file) {
+      const size = file.size / 1000000;
+
+      console.log(file)
+
+      if (size > 30) {
+        return toast.error("Sorry, file size should be less than 30MB");
+      }
+
+      const url = URL.createObjectURL(file);
+      setPreviewVideo(url);
+      setShowModal(true);
+
+      const data = await upload("video", file, Math.round(video.duration));
+      setUrl(data.url);
+      
+      setDuration(data.duration);
+      const ext = path.extname(data.url);
+      setThumbnail(data.url.replace(ext, ".jpg"));
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="video-upload">
+        <UploadIcon />
+      </label>
+      <input
+        style={{ display: "none" }}
+        id="video-upload"
+        type="file"
+        accept="video/*"
+        onChange={handleVideoUpload}
+      />
+      {showModal && (
+        <UploadVideoModal
+          closeModal={closeModal}
+          previewVideo={previewVideo}
+          thumbnail={thumbnail}
+          url={url}
+          duration={duration}
+        />
+      )}
+    </div>
+  );
+};
+
+export default UploadVideo;
