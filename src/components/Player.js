cdeae2e482e127d607bfdff38d3a5bc8bf1a53c9
jeffbebe <oskarw46@gmail.com>
2021-05-31 21:26:49 +0200

    Implemented algorithm changing quality

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,41 +8,51 @@ const Player = ({ previewUrl, speedInit }) => {
   const videoRef = useRef(null);
   let nSpeed = Math.round(speedInit);
 
-
   const dispatch = useDispatch();
-  let { id: videoId, url: src, thumb: poster, duration: duration } = useSelector(
-    (state) => state.video.data
-  );
+  let {
+    id: videoId,
+    url: src,
+    thumb: poster,
+    duration: duration,
+  } = useSelector((state) => state.video.data);
 
   const changeQuality = async () => {
-
     const vjsPlayer = videojs(videoRef.current);
 
-    let a = vjsPlayer.currentTime()
-    //src = "https://res.cloudinary.com/dtelr3fsv/video/upload/q_1/v1619367127/youtube/qgtotofykmh7q8h7rnqd.mp4"
-
-
-    vjsPlayer.src(src);
-    vjsPlayer.currentTime(a);
-    vjsPlayer.play();
-  }
+    let a = vjsPlayer.currentTime();
+    const temp = await client(
+      `${process.env.REACT_APP_BE}/videos/${videoId}/bitrate`
+    );
+    const bitrate = temp.data;
+    let intSpeed = nSpeed * 1024 * 1024;
+    console.log(bitrate);
+    console.log(intSpeed);
+    if (intSpeed < bitrate * 1.3) {
+      let q = String(Math.round((intSpeed / bitrate) * 10) * 10);
+      let position = q.indexOf("_");
+      src[position + 1] = q[0];
+      src[position + 2] = q[1];
+      vjsPlayer.src(src);
+      vjsPlayer.currentTime(a);
+      vjsPlayer.play();
+    }
+  };
 
   const fetchSpeed = async () => {
     const { data } = await client(`${process.env.REACT_APP_BE}/videos/speed`);
     nSpeed = Math.round(data);
-    console.log("(inside fetchSpeed) Network speed: " + nSpeed)
-
-  }
+    console.log("(inside fetchSpeed) Network speed: " + nSpeed);
+    changeQuality();
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetchSpeed();
-
-    }, 1000);
+    }, 5000);
     return () => {
       clearInterval(interval);
-    }
-  }, [])
+    };
+  }, []);
 
   useEffect(() => {
     const vjsPlayer = videojs(videoRef.current);
@@ -66,7 +76,6 @@ const Player = ({ previewUrl, speedInit }) => {
       }
     };
   }, [videoId, dispatch, src, previewUrl, poster]);
-
 
   return (
     <div data-vjs-player>
